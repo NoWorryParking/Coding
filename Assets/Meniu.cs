bfc880ea34a30bf527a35ca8c9e77fd654c81364
Anghelache Vlad <anghelache.vlad.1@gmail.com>
2021-06-09 08:22:47 +0300

    Added menu

---
+++ Assets/Meniu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Meniu : MonoBehaviour, IPointerDownHandler
+{
+    public static bool menuIsOpened = false;
+    public GameObject menu;
+    
+
+    
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(" Was Clicked.");
+        if (menuIsOpened)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    void Close()
+    {
+        menuIsOpened = false;
+        menu.SetActive(false);
+    }
+
+    void Open()
+    {
+        menuIsOpened = true;
+        menu.SetActive(true);
+    }
+}
