ad69a589acafd28d525e85796072369056b09aa6
Paula Iuga <paula.iuga@s.unibuc.ro>
2021-06-13 17:55:12 +0300

    Tried to fix build errors

---
+++ Assets/ListController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListController : MonoBehaviour
+{
+    [SerializeField] private GameObject listaIstoric;
+    [SerializeField] private GameObject listPrefab;
+    private void Start()
+    {
+        populateList();
+    }
+    public void populateList()
+    {
+        var rezervare = GameObject.Instantiate(listPrefab);
+       
+        rezervare.transform.SetParent(listaIstoric.transform);
+        rezervare.transform.localScale.Set(1f, 1f, 1f);
+    }
+}
